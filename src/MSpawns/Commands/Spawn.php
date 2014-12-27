884db07717cb7f04ee06490ef2d4d983037ed990
Flavius12 <info@evolsoft.tk>
2014-12-27 14:44:08 +0100

    Create Spawn.php

---
+++ src/MSpawns/Commands/Spawn.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * MSpawns (v1.4) by EvolSoft
+ * Developer: EvolSoft (Flavius12)
+ * Website: http://www.evolsoft.tk
+ * Date: 27/12/2014 01:26 PM (UTC)
+ * Copyright & License: (C) 2014 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/MSpawns/blob/master/LICENSE)
+ */
+
+namespace MSpawns\Commands;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\permission\Permission;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+use MSpawns\Main;
+
+class Spawn extends PluginBase implements CommandExecutor{
+	
+	public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+    
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+    	$fcmd = strtolower($cmd->getName());
+    	switch($fcmd){
+    			case "spawn":
+    				if($sender instanceof Player){
+    					if($sender->hasPermission("mspawns.spawn")){
+    						$this->plugin->teleportToSpawn($sender);
+    						return true;
+    					}
+    					else{
+    						$sender->sendMessage($this->plugin->translateColors("&", "&cYou don't have permissions to use this command"));
+    						return true;
+    					}
+    				}else{
+    					$sender->sendMessage($this->plugin->translateColors("&", Main::PREFIX . "&cYou can only perform this command as a player"));
+    					return true;
+    				}
+    				break;
+    		}
+    	return true;
+    }
+	
+}
+    ?>
