dc42f6676f42e2d1b3ed1f6e4cfad860955f8717
da123rrell <darrell.darel@gmail.com>
2017-08-12 20:50:48 +0700

    Update Hub.php

--- src/MSpawns/Commands/Hub.php
+++ src/MSpawns/Commands/Hub.php
@@ -32,7 +32,7 @@ class Hub extends PluginBase implements CommandExecutor{
         $this->plugin = $plugin;
     }
     
-    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) {
     	switch(strtolower($cmd->getName())){
     			case "hub":
     				if($sender instanceof Player){
